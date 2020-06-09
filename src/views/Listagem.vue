055633bf04160fba430991d884e809ea769fdeac
Victor <victor.brejo@gmail.com>
2020-06-09 17:08:22 -0300

    Telas

--- src/views/Listagem.vue
+++ src/views/Listagem.vue
@@ -1,9 +1,45 @@
 <template>
 <div>
-    LISTAGEM
+  <v-card
+    color="grey lighten-4"
+    flat
+    height="50px"
+    tile
+  >
+    <v-toolbar dense>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+
+  <div id="header"><img border="0" src="assets/JFW.jpg" width="170" height="140">
+<p> Portarias e Atos que regulamentam as atividades </p>
+<p>administrativas e judiciárias da SJPB desde 2006.</p>
+<br></div><br>
+
+<table>
+    <tr class="header"><th style="width:60%;">Portaria</th><th style="width:40%;">Descrição</th><th style="width:40%;"></th></tr>
+    <tr><td id="divisaoPortaria">PORTARIAS/ATOS 2020</td><td id="divisaoPortaria"> </td><td id="divisaoPortaria"> </td></tr>
+	<tr><td>Ato nº 199, 22 de maio de 2020</td><td>Dispõe acerca do regime de trabalho diferenciado e dos prazos processuais no âmbito do Tribunal Regional Federal da 5ª Região e das Seções Judiciárias vinculadas</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/Ato199.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr> 
+    <tr><td>Ato nº 199, 22 de maio de 2020</td><td>Dispõe acerca do regime de trabalho diferenciado e dos prazos processuais no âmbito do Tribunal Regional Federal da 5ª Região e das Seções Judiciárias vinculadas</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/Ato199.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr>  
+	<tr><td>Portaria nº 79, 22 de maio de 2020</td><td>Prorroga o prazo de vigência das Resoluções CNJ nº 313/2020, nº 314/2020 e nº 318/2020</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/Portaria79.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr>  
+	<tr><td>Edital nº 9, 20 de maio de 2020</td><td>INSPEÇÕES JUDICIAIS 2020 - ALTERAÇÃO</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/Edital092020DF.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr>  
+	<tr><td>Recomendação nº 66, 13 de maio de 2020</td><td>Recomenda aos Juízos com competência para o julgamento das ações que versem sobre o direito à saúde a adoção de medidas para garantir os melhores resultados à sociedade durante o período excepcional de pandemia da Covid-19.</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/RecomendaccaoCNJ66.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr>  
+</table>
 </div>
 </template>
-
-<script>
-
-</script>
