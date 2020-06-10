b0ca782fa87a4c332f939443ffd8e697c57394b6
Victor <victor.brejo@gmail.com>
2020-06-10 09:21:35 -0300

    Tela Portarias

--- src/views/Inserir.vue
+++ src/views/Inserir.vue
@@ -1,33 +1,57 @@
 <template>
-  <v-form>
+  <v-card class="mx-auto" style="max-width: 500px;">
+    <v-toolbar color="deep-purple accent-4" cards dark flat>
+      <v-card-title class="title font-weight-regular"
+        >Adicionar Portaria</v-card-title
+      >
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-form ref="form" v-model="form" class="pa-4 pt-6">
+      <v-text-field
+        v-model="titulo"
+        :rules="[rules.titulo]"
+        filled
+        color="deep-purple"
+        label="Portaria"
+        style="min-height: 96px"
+      ></v-text-field>
+      <v-textarea
+        v-model="descricao"
+        auto-grow
+        :rules="[rules.required]"
+        filled
+        color="deep-purple"
+        label="Descrição"
+        rows="5"
+      ></v-textarea>
+      <v-file-input :rules="[rules.arquivo]" label="Selecionar portaria"></v-file-input>
+    </v-form>
     <v-divider></v-divider>
-    <h1>Titulo</h1>
-    <v-text-field
-      label="Subject"
-      value="Plans for the weekend"
-      single-line
-      full-width
-      hide-details
-    ></v-text-field>
-    <v-divider></v-divider>
-    <h1>Portaria</h1>
-    <v-textarea
-      v-model="title"
-      label="Message"
-      full-width
-      single-line
-    ></v-textarea>
-  </v-form>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn
+        :disabled="!form"
+        class="white--text"
+        color="deep-purple accent-4"
+        depressed
+        >Enviar</v-btn
+      >
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        selected: ['Trevor Handsen'],
-        items: ['Trevor Handsen', 'Alex Nelson'],
-        title: 'Hi,\nI just wanted to check in and see if you had any plans the upcoming weekend. We are thinking of heading up to Napa',
-      }
+export default {
+  data: () => ({
+    form: false,
+    titulo: undefined,
+    descricao: undefined,
+    arquivo: undefined,
+    rules: {
+      required: (v) => !!v || "Descrição necessaria",
+      titulo: (v) => !!v || "Texto Necessario",
+      arquivo: (v) => !!v || "Arquivo Necessario",
     },
-  }
+  }),
+};
 </script>
--- src/views/Listagem.vue
+++ src/views/Listagem.vue
@@ -1,45 +1,28 @@
 <template>
-<div>
-  <v-card
-    color="grey lighten-4"
-    flat
-    height="50px"
-    tile
-  >
-    <v-toolbar dense>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <div id="app">
+    <head>
+    <title>índice de Portarias</title>
+    </head>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+    <body>
+    <div class="top aui-helper-clearfix">
+		<ul class="aui-helper-clearfix top-nav">
+			<li ><a href="http://www.trf5.jus.br/" target="_BLANK">TRF5</a></li>
+			<li><a href="http://www.jfpe.jus.br/" target="_BLANK">JFPE</a></li>
+			<li class="active ico-jfpb"><a href="http://www.jfpb.jus.br/" target="_BLANK">JFPB</a></li>
+			<li><a href="http://www.jfce.jus.br/" target="_BLANK">JFCE</a></li>
+			<li><a href="http://www.jfse.jus.br/" target="_BLANK">JFSE</a></li>
+			<li><a href="http://www.jfal.jus.br/" target="_BLANK">JFAL</a></li>
+			<li><a href="http://www.jfrn.jus.br/" target="_BLANK">JFRN</a></li>
+		</ul> 			
+<br><br><br><br><br><br>
+</div><br>
 
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
-  </v-card>
-
-  <div id="header"><img border="0" src="assets/JFW.jpg" width="170" height="140">
+<div id="header"><img border="0" src="arquivos/jf_logo.jpg" width="170" height="140">
 <p> Portarias e Atos que regulamentam as atividades </p>
 <p>administrativas e judiciárias da SJPB desde 2006.</p>
 <br></div><br>
 
-<table>
-    <tr class="header"><th style="width:60%;">Portaria</th><th style="width:40%;">Descrição</th><th style="width:40%;"></th></tr>
-    <tr><td id="divisaoPortaria">PORTARIAS/ATOS 2020</td><td id="divisaoPortaria"> </td><td id="divisaoPortaria"> </td></tr>
-	<tr><td>Ato nº 199, 22 de maio de 2020</td><td>Dispõe acerca do regime de trabalho diferenciado e dos prazos processuais no âmbito do Tribunal Regional Federal da 5ª Região e das Seções Judiciárias vinculadas</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/Ato199.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr> 
-    <tr><td>Ato nº 199, 22 de maio de 2020</td><td>Dispõe acerca do regime de trabalho diferenciado e dos prazos processuais no âmbito do Tribunal Regional Federal da 5ª Região e das Seções Judiciárias vinculadas</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/Ato199.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr>  
-	<tr><td>Portaria nº 79, 22 de maio de 2020</td><td>Prorroga o prazo de vigência das Resoluções CNJ nº 313/2020, nº 314/2020 e nº 318/2020</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/Portaria79.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr>  
-	<tr><td>Edital nº 9, 20 de maio de 2020</td><td>INSPEÇÕES JUDICIAIS 2020 - ALTERAÇÃO</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/Edital092020DF.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr>  
-	<tr><td>Recomendação nº 66, 13 de maio de 2020</td><td>Recomenda aos Juízos com competência para o julgamento das ações que versem sobre o direito à saúde a adoção de medidas para garantir os melhores resultados à sociedade durante o período excepcional de pandemia da Covid-19.</td><td title="Baixe a portaria"><a href="arquivos/portarias2020/RecomendaccaoCNJ66.pdf"> <img border="0" src="arquivos/download-icon.jpg" width="20" height="20"> </a></td></tr>  
-</table>
-</div>
+    </body>
+  </div>
 </template>
